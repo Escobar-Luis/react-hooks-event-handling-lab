9f3e412cb4e97003bbba78557d3b6b71642f3d41
Luis Escobar <lae311@stern.nyu.edu>
2021-11-01 15:23:14 -0400

    commit tree 90f1efdbf6c9b2340e11c76810d20233ba5ae597

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe () {
+    function focus (e) {
+        console.log('Good!')
+    }
+    function blur (e) {
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <button onFocus={focus} onBlur={blur}>
+            Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
